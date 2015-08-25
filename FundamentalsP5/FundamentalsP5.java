a9ce7f0da4a190a9dd2cc0fc266cb5a21bff10e2
cmis <cmis@cmis-hs15.(none)>
2015-08-25 11:54:17 +0700

    Completed P5 started P6

--- FundamentalsP5/FundamentalsP5.java
+++ FundamentalsP5/FundamentalsP5.java
@@ -5,13 +5,23 @@ public class FundamentalsP5
         String fav1 = "The Dirties";
         String fav2 = "Kill Bill Vol. 1";
         String fav3 = "Royal Tenenbaums";
+        String favS1 = "Fight the Power";
+        String favS2 = "Wide Eyes";
+        String favS3 = "Drive Your Funky Soul";
         
         String[] myFav = {fav1, fav2, fav3};
+        String[] myFav2 = {favS1, favS2, favS3};
+        
         for(int index = 0; index < myFav.length; index++ )
             {
             System.out.println(myFav[index] + "\n");
+            }//end for
             
-            }//end for 
+        for(int index = 0; index < myFav2.length; index++ )
+            {
+            System.out.print(myFav2[index] + ", ");
+                
+            }
         }//end method Main
         
         
---
+++ FundamentalsP6/FundamentalsP6.java
@@ -0,0 +1,20 @@
+import java.lang.Math
+
+public class FundamentalsP6
+    {
+    public static void main ( String[] args)
+        {
+        Random rand = new Random();
+        int[] num = 
+        for(int i = 1; i <=50; i++ )
+          {
+          
+                
+                
+          }
+            
+            
+        }//end method Main
+        
+        
+    }//end class FundamentalsP6
