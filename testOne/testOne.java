b8378057449d82f3fc0fab61c828de8ce1a7e108
cmis <cmis@cmis-hs15.(none)>
2015-09-04 11:42:59 +0700

    testOne Completed

--- testOne/testOne.java
+++ testOne/testOne.java
@@ -7,11 +7,10 @@ public class testOne
         String a = JOptionPane.showInputDialog("Input an Integer: ");
         int numA = Integer.parseInt(a);
         double[] arrayA = new double[numA];
-        Random rand = new Random();
-        int randInt = rand.nextInt((50 - 1) + 1) + 1;
-        double randDouble = (double) randInt;
+        
         for (int index = 0; index < arrayA.length; index++)
             {
+            double randDouble = Math.random() * (50 - 1) + 1 ;
             arrayA[index] = randDouble;
             }//end for
         for (int index = 0; index < arrayA.length; index++)
@@ -19,7 +18,29 @@ public class testOne
             System.out.println(index + ": " + arrayA[index]);
                 
             }//end for
+        for (int index = (arrayA.length - 1); index >= 0; index--)
+            {
+            int intVal = (int) arrayA[index];
+            if (intVal % 2 == 0)
+                {
+                String e = "E";
+                for (int i = 1; i < intVal; i++)   
+                    {
+                    e += "E";
+                    }//end for
+                System.out.println(index + ": " + e + arrayA[index]);     
+                }//end if
+            else
+                {
+                String o = "O";
+                for (int i = 1; i < intVal; i++)
+                    {
+                    o += "O";
+                    }//end for
+                System.out.println(index + ": " + o + arrayA[index]);
+                }//end eles
             
+            }//end for   
         }//end method Main
         
         
