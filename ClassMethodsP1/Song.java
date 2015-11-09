991518420552c88c0d72c8fa34e8af873dfa30c6
Nathan LeSeure <nathan1917-cmis@github.com>
2015-11-09 09:55:25 +0700

    Commiting

--- ClassMethodsP1/Song.java
+++ ClassMethodsP1/Song.java
@@ -9,25 +9,28 @@ public class Song
         title = "Fight the Power";
         length = 5.24;
         views = "7,657,845";
+        yearReleased = 1990;
         }//end Song
-    public Song( String title, double length, int year)
+    public Song( String title, double length, int yearReleased, String views)
         {
         this.title = title;
         this.length = length;
-        this.year = year;
+        this.yearReleased = yearReleased;
+        this.views = views;
         }//end Song
     public String toString()
         {
         String output = new String();
-        output = "Title: " + title + "\nLength: " + length + "\nViews: " + views;
+        output = "Title: " + title + "\nLength: " + length + "\nYear Released: " + yearReleased + "\nViews: " + views;
         return output;
         }//end to String
-    public getYearReleased()
+    public int getYearReleased()    
         {
-        return getYearReleased;
+        int output = yearReleased;
+        return output;
         }//end getYearReleased
-    public setYearReleased(int yearReleased)
+    public void setYearReleased(int yearReleased)
         {
-        this.yearRe
+        this.yearReleased = yearReleased;
         }//end setYearReleased
-    }//end class Song
+        }//end class Song
--- ClassMethodsP1/SongDriver.java
+++ ClassMethodsP1/SongDriver.java
@@ -4,7 +4,7 @@ public class SongDriver
         {
         Song song1 = new Song();
         System.out.println(song1);
-        Song song2 = new Song("Prophets of Rage", 3.19, "13,425");
-        System.out.println(song2);
+        Song song2 = new Song("Prophets of Rage", 3.19, 1988, "13,425");
+        System.out.println("\n" + song2);
         }//end method main
     }//end class SongDriver
